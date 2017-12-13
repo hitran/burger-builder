b61da5cc60450f3416ee91ee5f75ed5aa6920ca3
Tran Thuc Hi <tranthuchi@Trans-MacBook-Air.local>
2017-12-13 20:14:08 +0700

    adding effect on hide/show button

---
+++ Documents/job/UFE/js/javascript.js
@@ -0,0 +1,11 @@
+/**
+ * Created by tranthuchi on 12/13/17.
+ */
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $(".drop-down-fact").hide();
+    });
+    $("#show").click(function(){
+        $(".drop-down-fact").show();
+    });
+});
