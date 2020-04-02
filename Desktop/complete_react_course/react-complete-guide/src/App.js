e0eccb95d178bdcb7a8f9f0fea2e6c2972b9df48
Hi Tran <thuchitran@gmail.com>
2020-04-02 12:01:56 +0700

    initial commit

---
+++ Desktop/complete_react_course/react-complete-guide/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React Developer</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
